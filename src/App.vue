8be00d9f4422c80388a254e89b6f1e29509518dc
friwid <50137120+friwid@users.noreply.github.com>
2024-01-18 09:20:03 +0100

    Add form (CreatePage.vue) to add new pages

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,15 @@
   </header>
 
   <main>
-    <page-viewer
+<!--     <page-viewer
       v-if="pages.length > 0"
       :page="pages[activePage]"
-    ></page-viewer>
+    ></page-viewer> -->
+
+  <create-page
+    :page-created="pageCreated"
+  ></create-page>
+
   </main>
 
   <footer></footer>
@@ -19,13 +24,14 @@
 
 <script>
 import Navbar from "./components/Navbar.vue";
-
 import PageViewer from "./components/PageViewer.vue";
+import CreatePage from "./components/CreatePage.vue";
 
 export default {
   components: {
     Navbar,
     PageViewer,
+    CreatePage
   },
   created() {
     this.getPages();
@@ -39,7 +45,7 @@ export default {
   data() {
     return {
       activePage: 0,
-      pages: [],
+      pages: []
     };
   },
   methods: {
@@ -49,6 +55,9 @@ export default {
 
       this.pages = data;
     },
+    pageCreated(pageObj) {
+      console.log(pageObj);
+    }
   },
 };
 </script>
