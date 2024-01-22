adaf02d94434f8d42244a6b98f3016180fd33bdd
friwid <50137120+friwid@users.noreply.github.com>
2024-01-22 11:50:44 +0100

    Add EditPage view, accessible via PageList table

--- src/views/PagesList.vue
+++ src/views/PagesList.vue
@@ -1,12 +1,11 @@
 <template>
   <h4>Pages</h4>
   <div class="text-end">
-    <router-link
-      to="/pages/create"
-      class="btn btn-primary btn-sm"
-    >New Page</router-link>
+    <router-link to="/pages/create" class="btn btn-primary btn-sm"
+      >New Page</router-link
+    >
   </div>
-  <table class="table table-striped table-hover">
+  <table class="table table-hover">
     <thead>
       <tr>
         <th>Title</th>
@@ -18,6 +17,7 @@
       <tr
         v-for="(page, index) in $pages.getAllPages()"
         :key="index"
+        @click="goToPage(index)"
       >
         <td>{{ page.pageTitle }}</td>
         <td>{{ page.link.text }}</td>
@@ -29,8 +29,18 @@
 
 <script setup>
 import { ref, reactive, inject } from "vue";
+import { useRouter } from "vue-router";
 
-const $pages = inject('$pages');
+const $pages = inject("$pages");
+const router = useRouter();
 
-
+function goToPage(index) {
+  router.push({ path: `pages/${index}/edit` });
+}
 </script>
+
+<style scoped>
+.table.table-hover tr:hover {
+  cursor: pointer;
+}
+</style>
